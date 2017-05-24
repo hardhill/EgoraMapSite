d2426fb2401ffe77b26d0c0cd79b849b81c8ca00
hardhill <hardhill@mail.ru>
2017-05-25 02:30:36 +0800

    контроллер добавления маршрута

--- EgoraMap/Controllers/RoutesController.cs
+++ EgoraMap/Controllers/RoutesController.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using EgoraMap.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using System.Net;
 
 namespace EgoraMap.Controllers
 {
@@ -15,12 +19,13 @@ namespace EgoraMap.Controllers
     {
        
         DbEgoraContext db = new DbEgoraContext();
+        IHostingEnvironment _appEnvironment;
 
         List<ViewRoute> vrList = new List<ViewRoute>();
-        public RoutesController(DbEgoraContext context)
+        public RoutesController(DbEgoraContext context, IHostingEnvironment appEnvironment)
         {
             //this.db = context;
-            
+            _appEnvironment = appEnvironment;
             if (db.Routes.Any())
             {
                 foreach (Route route in db.Routes)
@@ -57,6 +62,76 @@ namespace EgoraMap.Controllers
             return Ok(vr);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> AddEgoraMap(IFormFile uploadImage, IFormFile uploadKML, IFormFileCollection ffile)
+        {
+            string urlimg, urlkml;
+            Route route = new Route();
+            try
+            {
+                // получаем имя файла
+                string fileNameImage = System.IO.Path.GetFileName(uploadImage.FileName);
+                string fileNameKML = System.IO.Path.GetFileName(uploadKML.FileName);
+
+                string strName = HttpContext.Request.Form["Name"];
+                string strDescription = HttpContext.Request.Form["Description"];
+                int arrPhoto = Request.Form.Files.Count;
+                urlimg = String.Format("{0}_{1}{2}", DateTime.Now.ToString("yyyyMMddHHmmssfff"), Guid.NewGuid(), Path.GetExtension(fileNameImage));
+                urlkml = String.Format("{0}_{1}{2}", DateTime.Now.ToString("yyyyMMddHHmmssfff"), Guid.NewGuid(), Path.GetExtension(fileNameKML));
+                
+                route.Name = strName;
+                route.Description = strDescription;
+                route.RouteImage = urlimg;
+                route.RouteKML = urlkml;
+            }
+            catch(Exception e)
+            {
+                return Forbid("Добавление записи не выполнено." + e.Message.ToString());
+            }
+
+            try
+            {
+                // сохраняем файл в папку Files в проекте
+                string pathimg = "/Files/Img/" + urlimg;
+                string pathkml = "/Files/Kml/" + urlkml;
+                using(var fileStream = new FileStream(_appEnvironment.WebRootPath + pathimg, FileMode.Create))
+                {
+                    await uploadImage.CopyToAsync(fileStream);
+                }
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + pathkml, FileMode.Create))
+                {
+                    await uploadKML.CopyToAsync(fileStream);
+                }
+                if (ffile != null)
+                {
+                    foreach(var photoFile in ffile)
+                    {
+                        string photoNameImage = System.IO.Path.GetFileName(photoFile.FileName);
+                        string urlphoto = String.Format("{0}_{1}{2}", DateTime.Now.ToString("yyyyMMddHHmmssfff"), Guid.NewGuid(), Path.GetExtension(photoNameImage));
+                        Photo photo = new Photo();
+                        photo.PhotoName = urlphoto;
+                        photo.Photocreated = DateTime.Now;
+                        photo.Description = photoNameImage;
+                        route.Photos.Add(photo);
+                        using(var fileStream = new FileStream(_appEnvironment.WebRootPath + urlphoto, FileMode.Create))
+                        {
+                            await photoFile.CopyToAsync(fileStream);
+                        }
+                    }
+                }
+                db.Entry(route).State = EntityState.Added;
+                db.SaveChanges();
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return Forbid("Добавление записи не выполнено."+e.Message.ToString());
+            }
+            
+
+           
+        }
+
         private IEnumerable<string> GetPhotos(int id)
         {
             List<string> photopath = new List<string>();
@@ -74,5 +149,13 @@ namespace EgoraMap.Controllers
             return photopath;
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
